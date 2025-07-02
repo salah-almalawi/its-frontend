78c106eb1c80a81460db3a2477521ff6fd57d817
salah almalawi <152004352+salah-almalawi@users.noreply.github.com>
2025-07-02 18:05:21 +0300

    تعديل اسم ملف ReduxProvider

--- src/components/Providers/ReduxProvider.jsx
+++ src/components/Providers/ReduxProvider.jsx
@@ -4,9 +4,9 @@ import { Provider } from 'react-redux';
 import { store } from '@/store/store';
 
 export default function ReduxProvider({ children }) {
-    return (
-        <Provider store={store}>
-            {children}
-        </Provider>
-    );
+  return (
+    <Provider store={store}>
+      {children}
+    </Provider>
+  );
 }
